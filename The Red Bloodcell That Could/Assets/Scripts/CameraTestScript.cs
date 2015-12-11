0637404e77c557b3d9d24690950434f70c50fb60
JoeyNieuwkoop <joey.nieuwkoop@gmail.com>
2015-12-11 13:44:00 +0100

    Camera
    Added Camera rotation & Cardboard Screens

--- The Red Bloodcell That Could/Assets/Scripts/CameraTestScript.cs
+++ The Red Bloodcell That Could/Assets/Scripts/CameraTestScript.cs
@@ -3,15 +3,24 @@ using System.Collections;
 
 public class CameraTestScript : MonoBehaviour {
 
-    public float speed = 25.0f;
+    public float speed = 2.0f;
     public bool inverted = false;
 
+    Quaternion initialPositioning;
 
     private Vector3 lastMouse = new Vector3(255, 255, 255);
 	// Use this for initialization
 	void Start () 
     {
-	
+        GameObject camParent = GameObject.Find("CardboardMain");
+        GameObject camChild = GameObject.Find("Head");
+        if (camParent != null && camChild != null)
+        {
+            camParent.transform.position = transform.position;
+            transform.parent = camChild.transform;
+            camChild.transform.parent = camParent.transform;
+            camParent.transform.Rotate(Vector3.right, 90);
+        }
 	}
 	
 	// Update is called once per frame
@@ -31,5 +40,8 @@ public class CameraTestScript : MonoBehaviour {
         if (Input.GetKey(KeyCode.A)) dir.x -= 1.0f;
 
         transform.Translate(dir * speed * Time.deltaTime);
+
+        //Quaternion rotation = new Quaternion(Input.gyro.attitude.x, Input.gyro.attitude.y, -Input.gyro.attitude.z, -Input.gyro.attitude.w);
+        //transform.localRotation = rotation;
 	}
 }
