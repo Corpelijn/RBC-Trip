233e8e814a68a437f252a00b5df4a6424df44909
metalfiredemon <ruud.hagens@hotmail.com>
2015-12-11 11:59:15 +0100

    zuurstof zooi

---
+++ The Red Bloodcell That Could/Assets/Scripts/ParticleShooter.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleShooter : MonoBehaviour {
+
+	public GameObject doel;
+	public GameObject particleHolder;
+	public ParticleSystem oxygen;
+
+	private float distance;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		distance = Vector3.Distance (transform.position, doel.transform.position);
+		if (Input.GetMouseButtonUp(0)) {
+			if (oxygen.isPlaying) {
+				oxygen.Stop();
+			}
+		}
+		OnMouseDown ();
+	}
+
+	void OnMouseDown() {
+		if (distance < 10) {
+			if (!oxygen.isPlaying) {
+				oxygen.Play();
+			}
+		}
+		else {
+
+		}
+	}
+}
