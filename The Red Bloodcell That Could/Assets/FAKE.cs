684aca2f9a0d784db5bffc68515be2bce22df6b3
KittyvdP <kittyvdp@gmail.com>
2016-01-22 10:51:30 +0100

    particle rood fixed

---
+++ The Red Bloodcell That Could/Assets/FAKE.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FAKE : MonoBehaviour {
+
+    public GameObject bloodcell;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = bloodcell.transform.position;
+	}
+}
